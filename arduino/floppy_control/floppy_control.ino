be8db2a55fa1753cfe6f8a632969adc35678cd79
Dan Stelljes <dan@stellj.es>
2015-09-22 21:26:02 -0500

    Fixed drive control loop (hopefully)

--- arduino/floppy_control/floppy_control.ino
+++ arduino/floppy_control/floppy_control.ino
@@ -1,30 +1,27 @@
 #include <TimerOne.h>
 
 // timing loop period (microseconds)
-#define LOOP_RESOLUTION 40;
+const byte LOOP_RESOLUTION = 40;
 
 // head position range
-#define HEAD_MIN 0;
-#define HEAD_MAX 150;
+const byte HEAD_MIN = 0;
+const byte HEAD_MAX = 150;
 
 // number of drives
-#define DRIVE_COUNT 1;
+const byte DRIVE_COUNT = 1;
 
 // starting step control pin
-#define STARTING_PIN 2;
+const byte STARTING_PIN = 2;
 // we assume step control pins occur on evens and direction pins occur on odds
 // (so for drive 1, step control is pin 4 and direction is pin 5)
-
-// interesting Stack Overflow discussion of #define vs. static const:
-// http://stackoverflow.com/questions/1674032/static-const-vs-define-in-c
 
 // head directions (by drive):
 byte directions[] = {
   LOW
 };
 
-// active frequencies (by drive):
-int frequencies[] = {
+// active periods (by drive):
+int periods[] = {
   0
 };
 
@@ -44,57 +41,51 @@ void setup() {
   pinMode(2, OUTPUT); // 0: step control
   pinMode(3, OUTPUT); // 0: direction
 
+  // initialize serial interface
+  Serial.begin(9600);
+
   // set the drives to starting position if they aren't already
   resetDrives();
-  delay(2000);
+
+  // @todo remove this, it's for testing
+  // should give us a tuning A on drive 0
+  setFrequency(0, 440);
 
   // initialize the timing loop
   Timer1.initialize(LOOP_RESOLUTION);
   Timer1.attachInterrupt(tick);
-
-  // initialize serial interface
-  Serial.begin(9600);
 }
 
 void loop() {
   // everything is done within the tick!
 }
 
-// @todo this totally doesn't work... the commented stuff is what the final
-// working result should eventually look like
+// steps drives according to their frequencies
 void tick() {
-  step(0);
-
-  /*
   for (byte drive = 0; drive < DRIVE_COUNT; drive++) {
-    if (frequencies[drive] > 0) {
+    if (periods[drive] > 0) {
       ticks[drive]++;
 
-      if (ticks[drive] >= 4) {
+      if (ticks[drive] >= periods[drive]) {
         step(drive);
         ticks[drive] = 0;
       }
     }
   }
-  */
 }
 
-// pulses a pin (because we're lazy)
-void pulse(byte pin) {
-  digitalWrite(pin, HIGH);
-  digitalWrite(pin, LOW);
-}
-
-// steps a head (like pulse, but checks for direction):
+// steps a head
 void step(byte drive) {
   byte d = getDirectionPin(drive);
   byte s = getStepPin(drive);
 
   if (positions[drive] >= HEAD_MAX) {
     directions[drive] = HIGH;
+    Serial.println("Switched HIGH");
   }
   else if (positions[drive] <= HEAD_MIN) {
     directions[drive] = LOW;
+    Serial.println("Switched LOW");
   }
 
   if (directions[drive] == HIGH) {
@@ -114,19 +105,29 @@ void resetDrives() {
   // set everything we track to 0
   for (byte drive = 0; drive < DRIVE_COUNT; drive++) {
     directions[drive] = LOW;
-    frequencies[drive] = 0;
+    periods[drive] = 0;
     positions[drive] = 0;
     ticks[drive] = 0;
   }
 
   // the drives won't go past 0, so that's cool
-  for (byte step = HEAD_MAX; step >= HEAD_MIN; step--) {
+  for (byte step = HEAD_MIN; step <= HEAD_MAX; step++) {
     for (byte drive = 0; drive < DRIVE_COUNT; drive++) {
       digitalWrite(getDirectionPin(drive), HIGH);
-      pulse(getStepPin(drive));
+      digitalWrite(getStepPin(drive), HIGH);
+      digitalWrite(getStepPin(drive), LOW);
     }
 
     delay(5);
+  }
+}
+
+void setFrequency(byte drive, int frequency) {
+  if (frequency == 0) {
+    periods[drive] = 0;
+  }
+  else {
+    periods[drive] = 1000000 / frequency / LOOP_RESOLUTION;
   }
 }
 
