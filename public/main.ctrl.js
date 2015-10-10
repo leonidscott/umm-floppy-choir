9df67c9e97bb6bbf744b6fea9b0f8281a351b547
gensc004 <gensc004@morris.umn.edu>
2015-10-10 11:37:49 -0500

    Made simple ui and event handling for keydown in textarea

--- public/main.ctrl.js
+++ public/main.ctrl.js
@@ -1,3 +1,36 @@
-angular.module('app').controller("MainController", function(){
-    var vm = this;
-});
+'use strict';
+
+angular.module('app').controller("MainController", ['$scope', '$http', function($scope, $http){
+
+	$scope.flat = false;
+	$scope.sharp = false;
+	var keyMap = {
+    		'65': 'a',
+    		'66': 'b',
+    		'67': 'c',
+    		'68': 'd',
+    		'69': 'e',
+    		'70': 'f',
+    		'71': 'g'
+    	}
+    $scope.startMusic = function() {
+    	console.log('WE STARTED SOMETHING BBY');
+    }
+
+    $scope.stopMusic = function() {
+    	console.log('WE STOPPPED IT')
+    }
+
+    $scope.triggerNote = function(event) {
+    	//Determine note
+    	var keyPressed = keyMap["" + event.keyCode];
+       	if(keyPressed) {
+       	}
+
+
+
+    	//Send note to server
+
+
+    }
+}]);
