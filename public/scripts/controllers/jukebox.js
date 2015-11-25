ab449c255332b16f7e83f342b4fa20628c848779
harre096 <harre096@umn.edu>
2015-11-24 22:39:25 -0600

    switching play/pause button to match expectation

--- public/scripts/controllers/jukebox.js
+++ public/scripts/controllers/jukebox.js
@@ -3,51 +3,57 @@
 angular.module('app').controller('JukeboxController', function($scope, socket) {
 	$scope.playing = null;
 	$scope.songs = {};
-	$scope.showPause = false;
+	$scope.showPause = true;
 	$scope.nothingIsPlaying = true;
 
+//Fetch song list, load songs after response
   socket.emit('get song list');
 
 	socket.on('song list', function(songs) {
 		$scope.songs = songs;
 	});
 
-	socket.on('song changed', function(song) {
-		$scope.playing = song;
-		$scope.showPause = false;
-	});
-
-	socket.on('song paused', function() {
-		$scope.showPause = true;
-	})
-
-	socket.on('song played', function() {
-		$scope.showPause = false;
-	})
-
+//Means by which a user interacts with the page
 	$scope.start = function(id) {
 		socket.emit('start song', id);
-		$scope.showPause = false;
+		$scope.showPause = true;
 	};
+
+	//Unused, not sure if backend is actually implemented
+	// $scope.stop = function() {
+	// 	socket.emit('stop');
+	// };
+
+	$scope.switchPauseOrPlay = function() {
+		if($scope.showPause) { //if pause is clicked...
+			$scope.pause();
+		} else {
+			$scope.play();
+		}
+	}
 
 	$scope.play = function() {
 		socket.emit('play');
-		$scope.showPause = false;
+		$scope.showPause = true;
 	};
 
 	$scope.pause = function() {
 		socket.emit('pause');
+		$scope.showPause = false;
 	};
 
-	$scope.stop = function() {
-		socket.emit('stop');
-	};
+//Recieved from server as a result of other user's interations
+	socket.on('song changed', function(song) {
+		$scope.playing = song;
+		$scope.showPause = true;
+	});
 
-	$scope.switchPauseOrPlay = function() {
-		if($scope.showPause) {
-			$scope.play();
-		} else {
-			$scope.pause();
-		}
-	}
+	socket.on('song paused', function() {
+		$scope.showPause = false;
+	})
+
+	socket.on('song played', function() {
+		$scope.showPause = true;
+	})
+
 });
