c29eae4c8e341b4f16447e6336259c1479514c2c
Dan Stelljes <stell124@morris.umn.edu>
2015-12-16 20:35:43 -0600

    Refactored jukebox frontend

--- public/scripts/controllers/jukebox.js
+++ public/scripts/controllers/jukebox.js
@@ -1,59 +1,22 @@
 'use strict';
 
 angular.module('app').controller('JukeboxController', function($scope, socket) {
-	$scope.playing = null;
-	$scope.songs = {};
-	$scope.showPause = true;
-	$scope.nothingIsPlaying = true;
 
-//Fetch song list, load songs after response
-  socket.emit('get song list');
-
-	socket.on('song list', function(songs) {
-		$scope.songs = songs;
-	});
-
-//Means by which a user interacts with the page
-	$scope.start = function(id) {
-		socket.emit('start song', id);
+	$scope.queue = function(id) {
+		socket.emit('queue', id);
 		$scope.showPause = true;
 	};
 
-	//Unused, not sure if backend is actually implemented
-	// $scope.stop = function() {
-	// 	socket.emit('stop');
-	// };
-
-	$scope.switchPauseOrPlay = function() {
-		if($scope.showPause) { //if pause is clicked...
-			$scope.pause();
-		} else {
-			$scope.play();
-		}
-	}
+	$scope.stop = function() {
+	  socket.emit('stop');
+	};
 
 	$scope.play = function() {
 		socket.emit('play');
-		$scope.showPause = true;
 	};
 
 	$scope.pause = function() {
 		socket.emit('pause');
-		$scope.showPause = false;
 	};
-
-//Recieved from server as a result of other user's interations
-	socket.on('song changed', function(song) {
-		$scope.playing = song;
-		$scope.showPause = true;
-	});
-
-	socket.on('song paused', function() {
-		$scope.showPause = false;
-	})
-
-	socket.on('song played', function() {
-		$scope.showPause = true;
-	})
 
 });
