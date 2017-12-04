d7f8f6b326abfba66b5d5451a923d609f3eac770
Dan Stelljes <stell124@morris.umn.edu>
2017-12-03 20:16:21 -0600

    Updated deps and started cleaning up library

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -18,26 +18,7 @@ angular.module('app').controller('MainController', function($scope, $location, s
 		return display.route === $location.path();
 	};
 
-	socket.on('set scope', function(scope) {
-		for (var key in scope) {
-			$scope[key] = scope[key];
-		}
-	});
-
-	socket.on('update scope', function(path, value) {
-		var components = path.split('.');
-		var property = components.pop();
-
-		var context = $scope;
-
-		for (var i = 0, key; context && (key = components[i]); i++) {
-			if (!(key in context)) {
-				context[key] = {};
-			}
-
-			context = context[key];
-		}
-
-		context[property] = value;
+	socket.on('set state', function(state) {
+		Object.assign($scope, state);
 	});
 });
